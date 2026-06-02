f5c21a6ee83f68b075d82a20214d0a9d12ad00f2
sweta0618-afk <sweta0618@gmail.com>
2026-06-03 00:15:12 +0600

    Update server.js

--- server.js
+++ server.js
@@ -48,9 +48,9 @@ app.post('/api/recognize-food', async (req, res) => {
     "c": число
 }`;
 
-        // Отправка запроса в Gemini 2.5-flash,
+        // Отправка запроса в Gemini 2.5 Flash
         const response = await ai.models.generateContent({
-            model: 'gemini-2.5-flash,
+            model: 'gemini-2.5-flash',
             contents: [
                 prompt,
                 { inlineData: { data: base64Data, mimeType: 'image/jpeg' } }
